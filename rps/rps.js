2f8dc8be9f8e65e380c747e19c3c97352c3cfd5a
2bumb <alee62580@gmail.com>
2024-03-14 16:43:39 -0700

    added changes

---
+++ rps/rps.js
@@ -0,0 +1,10 @@
+
+// our declarations 
+
+const choices = ["rock", "paper", "scissors"];
+
+const playerDisplay = document.getElementById("playerDisplay");
+
+const computerDisplay = document.getElementById("computerDisplay");
+
+const gameresult = document.getElementById("gameResult");
