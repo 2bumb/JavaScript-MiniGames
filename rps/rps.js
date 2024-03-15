88e70a6d931fc013989bfb9f407c77176efd59ab
2bumb <alee62580@gmail.com>
2024-03-14 18:05:36 -0700

    added changes

--- rps/rps.js
+++ rps/rps.js
@@ -1,5 +1,5 @@
 
-// our declarations 
+// declaring the variables 
 
 const choices = ["rock", "paper", "scissors"];
 
@@ -8,3 +8,14 @@ const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 
 const gameresult = document.getElementById("gameResult");
+
+
+
+// keep in mind javascript is cap sensitive
+
+function playgame(playerChoice){
+
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+  console.log(computerChoice);
+};
