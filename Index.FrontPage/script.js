f25edda6142776eba52adac19b7a12e6898b0d17
2bumb <alee62580@gmail.com>
2024-03-24 19:21:14 -0700

    added changes

--- Index.FrontPage/script.js
+++ Index.FrontPage/script.js
@@ -1,23 +1,5 @@
-
-
-
-// This function creates a sound when user clicks on the cards
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var rpsLink = document.getElementById("rpsLink");
-    var audio = document.getElementById("homecardAudio");
-    
-    // Adding click event listener to the Rock Paper Scissors link
-    rpsLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent default link behavior
-        
-        // Play audio
-        audio.play();
-        
-        // When the audio ends, navigate to the linked page
-        audio.addEventListener('ended', function() {
-            window.location.href = rpsLink.getAttribute("href");
-        });
-    });
+document.getElementById("rpsLink").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    document.getElementById("homecardAudio").play(); // Play audio
+    setTimeout(() => window.location.href = this.getAttribute("href"), 500); // Navigate to the linked page after a delay
 });
