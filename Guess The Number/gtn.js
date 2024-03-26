15f5760bbac88010af172a12d461364a3f70d49b
2bumb <alee62580@gmail.com>
2024-03-26 15:12:45 -0700

    when 21 is hovered, the audio meme21audio will be played

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -5,8 +5,7 @@ const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 
 console.log(answer);
-
-
+//when the number 21 is hovered, the audio with the id "meme21audio" wil be played
 function play21hover(){
    document.querySelector('.tilted-left').addEventListener('mouseover', function() {
     document.getElementById('meme21Audio').play();
