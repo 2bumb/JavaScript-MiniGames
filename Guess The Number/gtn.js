e27dc79a1e115dae3e9f71e08449f10917e7de3e
2bumb <alee62580@gmail.com>
2024-03-26 15:10:06 -0700

    added changes

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -5,3 +5,11 @@ const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 
 console.log(answer);
+
+
+function play21hover(){
+   document.querySelector('.tilted-left').addEventListener('mouseover', function() {
+    document.getElementById('meme21Audio').play();
+});
+}
+play21hover();
