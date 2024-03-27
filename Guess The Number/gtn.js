50a34d3864e72d18055e951d79fffeedadb5f25b
2bumb <alee62580@gmail.com>
2024-03-26 20:52:16 -0700

    added changes

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -3,6 +3,19 @@ const maxNum = 100;
 // understand how math we're using
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
+let attempts = 0;
+let guess;
+let running;
+
+while(running){
+        guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+console.log(typeof guess, guess);
+    running=false;
+}
+
+
+
+
 
 console.log(answer);
 //when the number 21 is hovered, the audio with the id "meme21audio" wil be played
