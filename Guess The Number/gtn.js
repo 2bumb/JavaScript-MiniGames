7b2bcd284232e9620fd00b7c6b61bda1a0ce7159
2bumb <alee62580@gmail.com>
2024-03-27 20:29:32 -0700

    added changes

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -1,30 +1,37 @@
-const minNum = 1;
-const maxNum = 100;
+// const minNum = 1;
+// const maxNum = 100;
 // understand how math we're using
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-let attempts = 0;
-let guess;
-let running;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-while(running){
-        guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-        guess= Number(guess);
-console.log(typeof guess, guess);
-    running=false;
+// let attempts = 0;
+// let guess;
+// let running;
+
+// while(running){
+//         guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+//         guess= Number(guess);
+// console.log(typeof guess, guess);
+//     running=false;
 
 
-    guess = Number(guess);
-    if(isNaN(guess)){
+//     guess = Number(guess);
+//     if(isNaN(guess)){
 
         
-    }
+//     }
+// }
+
+// console.log(answer);
+
+
+
+function submitGuess() {
+    console.log("Submit button clicked");
+    var guess = document.getElementById("guessInput").value;
+    console.log("Submitted guess:", guess);
+    // Perform further actions
 }
-
-console.log(answer);
-
-
-
 
 
 
