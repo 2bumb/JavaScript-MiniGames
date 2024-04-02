7069f3cfd11b4c592dad904823a1e7df6691fcd1
2bumb <alee62580@gmail.com>
2024-04-01 20:21:37 -0700

    Added changes

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -1,5 +1,6 @@
 // Flag to check if the random number has been generated
 let randomNumberGenerated = false;
+let answer;
 
 //This code displays the clients number of guess using the button and if the user submits an answer without inputing an answer, there will be a message saying "Please enter a guess"
 
@@ -7,38 +8,37 @@ let randomNumberGenerated = false;
 function submitGuess() {
     const minNum = 1;
     const maxNum = 10;
-    let answer;
 
-    if (!randomNumberGenerated) { // Highlighted change
-        answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum; // Highlighted change
-        console.log(answer); // Highlighted change
-        randomNumberGenerated = true; // Set the flag to true after generating the random number // Highlighted change
-
-        // Display the answer in HTML
-        document.querySelector('.answer').innerText = "Answer"+" "+answer; // Highlighted change
+    if (!randomNumberGenerated) {
+        answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        console.log(answer);
+        randomNumberGenerated = true;
+        document.querySelector('.answer').innerText = "Answer" + " " + answer;
     }
 
-    let guessInput = document.getElementById("guessInput"); //targets the number input in html 
-    let guess = parseInt(guessInput.value); //targets the number value and stores number data //parseInt means we are converting our input which is a string by default into a number/integer 
+    let guessInput = document.getElementById("guessInput");
+    let guess = parseInt(guessInput.value);
 
-    if (isNaN(guess) || guess < 1 || guess > 10) { //if "if (isNaN(guess)"we are checking if something is not a number ex: hello is a string so it will be true || is means or.  guess < 1 || guess > 100) mean the guess must be less than 1 or greater than 100
+    if (isNaN(guess) || guess < 1 || guess > 10) {
         guessInput.setCustomValidity("Please enter a number between 1 and 10.");
         guessInput.reportValidity();
     } else {
-        // Your logic for submitting the guess
         console.log("Submitted guess:", guess);
-        
-        // Add the guess to the list of previous guesses
+
         var previousGuesses = document.getElementById("previousguest");
         var currentGuesses = previousGuesses.innerText;
         if (currentGuesses) {
             previousGuesses.innerText = currentGuesses + ", " + guess;
         } else {
-            previousGuesses.innerText ="Previous Guesses:" + " " + guess;
+            previousGuesses.innerText = "Previous Guesses:" + " " + guess;
         }
-        
-        // Clear the input field after submitting the guess
+
         guessInput.value = "";
+
+        // Check if the user's guess matches the randomly generated number
+        if (guess === answer) {
+            document.querySelector('.answer').innerText = "You win!!";
+        }
     }
 }
 
