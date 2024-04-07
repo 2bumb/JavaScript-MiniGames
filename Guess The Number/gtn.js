0e1866cda98dafedfd85b3f5bf40ee5cca2e78e9
2bumb <alee62580@gmail.com>
2024-04-06 21:32:37 -0700

    added changes

--- Guess The Number/gtn.js
+++ Guess The Number/gtn.js
@@ -41,7 +41,7 @@ function submitGuess() {
             document.querySelector('.answer').innerText = "You win!!"; //if user guess is the same as the answer, the player wins!!
             document.querySelector('.answer').style.fontSize = "4.5vmin";
         } else if (Math.abs(guess - answer) <= warmRange) { 
-            document.querySelector('.answer').innerText = "You're getting warm!"; // if the guess is close to the answer just for a reminder remeber putting the condition near the top will be priortize that's why the message of the code wasnt working correctly 
+            document.querySelector('.answer').innerText = "TSS!! You're getting warm!"; // if the guess is close to the answer just for a reminder remeber putting the condition near the top will be priortize that's why the message of the code wasnt working correctly 
             document.querySelector('.answer').style.fontSize = "2vmin";
         }else if (guess < answer) {
             document.querySelector('.answer').innerText = "BRR!! The number is too low"; //if the user guess is lower than answer, 
