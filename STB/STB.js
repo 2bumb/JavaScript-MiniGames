5d0021fa08c5094ad0aecccc14e4a02212fffa22
2bumb <alee62580@gmail.com>
2024-04-19 18:36:17 -0700

    added changes

--- STB/STB.js
+++ STB/STB.js
@@ -1,4 +1,33 @@
+const startrefreshdisplay = document.getElementById("startrefreshdisplay");
+const fasolidfaplay = document.getElementByClassName("fasolidfaplay");
 
+
+function startclick() {
+  const startButton = document.querySelector(".startbutton");
+  const playIcon = document.getElementById("playIcon");
+
+  // Change button background color to baby blue
+  startButton.style.backgroundColor = "rgb(68, 196, 255)";
+
+ ;
+
+  // Update button text
+  document.getElementById("startrefreshdisplay").textContent = "32b";
+  document.getElementById("startrefreshdisplay").innerHTML = '<i class="fa-solid fa-play" style="color: white;"></i> 32b';
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+ //home button 
 
 function homeclick() { //when homeclick is clicked once the audio is over the user will  be placed to a new href
 
