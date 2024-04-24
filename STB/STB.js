b6d6971a08011a7b7ba44c98dacf47fbc4963571
2bumb <alee62580@gmail.com>
2024-04-23 18:59:14 -0700

    added changes

--- STB/STB.js
+++ STB/STB.js
@@ -47,7 +47,21 @@ document.addEventListener('keydown', spacebarPressHandler);
 document.addEventListener('keyup', spacebarReleaseHandler);
 
  
+function homeclick() { //when homeclick is clicked once the audio is over the user will  be placed to a new href
 
+
+  var homecardAudio = document.getElementById("homecardAudio");
+  
+  // Play the audio
+  homecardAudio.play();
+
+ 
+  homecardAudio.addEventListener('ended', function() {// ('ended', function() is a addEventListener which needs a event and also a function to run
+    window.location.href = "/Index.FrontPage/index.html";
+  });
+
+   
+}
 
   
   // function startclick() {
