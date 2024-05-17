ed2bd73605fa24287f4e23f4c3982e6b3ab74b40
2bumb <alee62580@gmail.com>
2024-05-16 20:13:47 -0700

    HSG: working on js functionality

--- HSG/hsg.js
+++ HSG/hsg.js
@@ -1,3 +1,65 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("scoretext");
+const resetBtn = document.querySelector("resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight =gameBoard.height;
+const boardBackground ="green";
+const snakeColor="lightgreen";
+const snakeBorder = "black";
+const foodColor= "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX=null;
+let foodY=null;
+let scoere = 0;//maybe null
+let snake = 
+
+[{x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize, y:0},
+    {x:0, y:0}]
+
+    window.addEventListener("keydown", changeDirection);
+    resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+function gameStart(){};
+
+function nextTick(){};
+
+function clearBoard(){};
+
+function createFood(){
+function randomFood(min,max){
+    const randNum = Math.round((Math.random() * (max-min) + min))
+    return randNum;
+}
+foodX = randomFood(0, gameWidth-unitSize);
+console.log(foodX);
+
+};
+
+function drawFood(){};
+
+function moveSnake(){};
+
+function drawSnake(){};
+
+function changeDirection(){};
+
+function checkGameOver(){};
+
+function displayGameOver(){};
+
+function resetGame(){};
+
+
+
+
 //addeventlistner, when client clicks on the home button, there will be audio playing. Once the audio is over/ended the client will be transformed to the index page/homepage 
 // Select the home button element
 //I need to understand why There's too much curly brackets 
