dd0bde133b328c0e06098b776187be5eb4ea9574
2bumb <alee62580@gmail.com>
2024-05-18 16:26:13 -0700

    HSG: created a localstorage so when the client refresh the browser, the highscore will be shown, inly if the client refresh via refresh button

--- HSG/hsg.js
+++ HSG/hsg.js
@@ -1,6 +1,7 @@
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("#scoretext");
+const highScoreText = document.querySelector("#highScoretext");
 const resetBtn = document.querySelector("#resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
@@ -15,6 +16,7 @@ let yVelocity = 0;
 let foodX;
 let foodY;
 let score = 0;
+let highScore = localStorage.getItem("highScore") ? parseInt(localStorage.getItem("highScore")) : 0;
 let snake = [
     { x: unitSize * 4, y: 0 },
     { x: unitSize * 3, y: 0 },
@@ -27,11 +29,11 @@ window.addEventListener("keydown", changeDirection);
 resetBtn.addEventListener("click", resetGame);
 
 gameStart();
-createFood();
 
 function gameStart() {
     running = true;
     scoreText.textContent = score;
+    highScoreText.textContent = highScore;
     clearBoard();
     createFood();
     drawFood();
@@ -51,19 +53,17 @@ function nextTick() {
         }, 100);
     } else {
         displayGameOver();
+        negativeaudio.play();
     }
 }
 
 function clearBoard() {
-    // Draw the background color
     ctx.fillStyle = boardBackground;
     ctx.fillRect(0, 0, gameWidth, gameHeight);
 
-    // Draw the grid
     ctx.strokeStyle = "gray";
     ctx.lineWidth = 0.5;
 
-    // Draw vertical grid lines
     for (let x = 0; x <= gameWidth; x += unitSize) {
         ctx.beginPath();
         ctx.moveTo(x, 0);
@@ -71,7 +71,6 @@ function clearBoard() {
         ctx.stroke();
     }
 
-    // Draw horizontal grid lines
     for (let y = 0; y <= gameHeight; y += unitSize) {
         ctx.beginPath();
         ctx.moveTo(0, y);
@@ -86,7 +85,6 @@ function createFood() {
         return randNum;
     }
     
-    // Ensure food does not overlap with the snake
     let foodOnSnake;
     do {
         foodOnSnake = false;
@@ -95,12 +93,11 @@ function createFood() {
 
         snake.forEach(part => {
             if (part.x === foodX && part.y === foodY) {
-                foodOnSnake = true; 
+                foodOnSnake = true;
+               
             }
         });
     } while (foodOnSnake);
-    
-    console.log(foodX, foodY);
 }
 
 function drawFood() {
@@ -115,20 +112,55 @@ function moveSnake() {
     if (snake[0].x === foodX && snake[0].y === foodY) {
         score += 1;
         scoreText.textContent = score;
-        createFood();
         eatAudio.play();
+        createFood();
     } else {
         snake.pop();
     }
 }
 
 function drawSnake() {
-    snake.forEach(snakePart => {
+    snake.forEach((snakePart, index) => {
         ctx.fillStyle = snakeColor;
         ctx.strokestyle = snakeBorder;
         ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
         ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+
+        if (index === 0) { // Draw eyes and tongue on the head
+            drawEyesAndTongue(snakePart.x, snakePart.y);
+        }
     });
+}
+
+function drawEyesAndTongue(x, y) {
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(x + unitSize * 0.25, y + unitSize * 0.25, unitSize * 0.1, 0, 2 * Math.PI);
+    ctx.arc(x + unitSize * 0.75, y + unitSize * 0.25, unitSize * 0.1, 0, 2 * Math.PI);
+    ctx.fill();
+
+    if (Math.random() < 0.1) {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(x + unitSize * 0.5, y + unitSize * 0.5);
+        switch (true) {
+            case yVelocity === -unitSize:
+                ctx.lineTo(x + unitSize * 0.5, y);
+                break;
+            case yVelocity === unitSize:
+                ctx.lineTo(x + unitSize * 0.5, y + unitSize);
+                break;
+            case xVelocity === -unitSize:
+                ctx.lineTo(x, y + unitSize * 0.5);
+                break;
+            case xVelocity === unitSize:
+                ctx.lineTo(x + unitSize, y + unitSize * 0.5);
+                break;
+        }
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
 }
 
 function changeDirection(event) {
@@ -173,38 +205,45 @@ function checkGameOver() {
             running = false;
         }
     }
+
+    if (!running) {
+        updateHighScore();
+    }
 }
+
+function updateHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem("highScore", highScore);
+        highScoreText.textContent = highScore;
+    }
+}
+
 function displayGameOver() {
     ctx.font = "30px 'Press Start 2P'";
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
     ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
-    negativeaudio.play();
-    running = false;
 }
 
 function resetGame() {
     animateReset(); // Trigger the animation
 }
-// This will  refresh the game, before the  game refresh, the animation of the icon will be played with audio, when those two are over the refresh will activate
+
 function animateReset() {
     const resetIcon = resetBtn.querySelector("i");
     resetIcon.classList.add("spin-animation");
 
     // Play the refresh audio
+    const refreshAudio = document.getElementById("refreshAudio");
     refreshAudio.play();
 
-    // Wait for the animation and audio to finish
     setTimeout(() => {
-        // Remove the animation class
         resetIcon.classList.remove("spin-animation");
-
-        // Wait for the audio to finish playing
         setTimeout(() => {
-            // Call the reset function after both animation and audio have finished
             doResetGame();
-        }, 500); // Adjust the time to match the duration of your audio
-    }, 500); // Adjust the time to match the duration of your animation
+        }, 500);
+    }, 500);
 }
 
 function doResetGame() {
@@ -218,74 +257,21 @@ function doResetGame() {
         { x: unitSize, y: 0 },
         { x: 0, y: 0 }
     ];
+    localStorage.setItem("highScore", 0);
+    highScore = 0;
+    highScoreText.textContent = highScore;
     gameStart();
 }
 
-
-//snake sticks out it's tongue out 
-function drawSnake() {
-    snake.forEach((snakePart, index) => {
-        ctx.fillStyle = snakeColor;
-        ctx.strokestyle = snakeBorder;
-        ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
-        ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
-
-        if (index === 0) { // Draw eyes and tongue on the head
-            drawEyesAndTongue(snakePart.x, snakePart.y);
-        }
-    });
-}
-
-function drawEyesAndTongue(x, y) {
-    // Draw eyes
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.arc(x + unitSize * 0.25, y + unitSize * 0.25, unitSize * 0.1, 0, 2 * Math.PI); // Left eye
-    ctx.arc(x + unitSize * 0.75, y + unitSize * 0.25, unitSize * 0.1, 0, 2 * Math.PI); // Right eye
-    ctx.fill();
-
-    // Randomly draw tongue
-    if (Math.random() < 0.1) { // 10% chance to stick out tongue
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.moveTo(x + unitSize * 0.5, y + unitSize * 0.5);
-        switch (true) {
-            case yVelocity === -unitSize: // Moving up
-                ctx.lineTo(x + unitSize * 0.5, y);
-                break;
-            case yVelocity === unitSize: // Moving down
-                ctx.lineTo(x + unitSize * 0.5, y + unitSize);
-                break;
-            case xVelocity === -unitSize: // Moving left
-                ctx.lineTo(x, y + unitSize * 0.5);
-                break;
-            case xVelocity === unitSize: // Moving right
-                ctx.lineTo(x + unitSize, y + unitSize * 0.5);
-                break;
-                
-        }
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-    }
-}
-
-
-//audio plays when clients clicks on the home button
 const homeButton = document.getElementById("homeBtn");
 
 homeButton.addEventListener("click", function () {
-    console.log("Home button clicked!");
-    var homecardAudio = document.getElementById("homecardAudio");
+    const homecardAudio = document.getElementById("homecardAudio");
 
     if (homecardAudio) {
-        console.log("Audio element found:", homecardAudio);
         homecardAudio.play();
         homecardAudio.addEventListener('ended', function () {
-            console.log("Audio ended, navigating to the new page...");
             window.location.href = "/Index.html/index.html";
         });
-    } else {
-        console.log("Audio element not found!");
     }
 });
